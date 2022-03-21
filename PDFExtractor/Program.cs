7d292a26f9b12539a9c1108bb37bcc29e579b63b
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2022-03-20 23:21:22 -0300

    Adicionar arquivos de projeto.

---
+++ PDFExtractor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string file = @"C:/Users/cleit/Downloads/boleto (2) (1).pdf";
+            using(PdfReader reader = new PdfReader(file))
+            {
+                for(int pageNo = 1; pageNo <=  reader.NumberOfPages; pageNo++)
+                {
+
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default,Encoding.UTF8,Encoding.Default.GetBytes(text)));
+                    sb.Append(text);
+                
+                }
+            }
+            
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
