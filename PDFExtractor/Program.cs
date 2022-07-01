beaca722795f462a1d0f3499fc3cec432a277b67
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2022-06-30 21:54:42 -0300

    Commit30062022

--- PDFExtractor/Program.cs
+++ PDFExtractor/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using Refit;
+using System.Drawing;
 
 namespace PDFExtractor // Note: actual namespace depends on the project name.
 {
@@ -17,39 +18,49 @@ namespace PDFExtractor // Note: actual namespace depends on the project name.
 
         static async Task Main(string[] args)
         {
-            try
-            {
-                var CategoriaDocumentoClient = RestService.For <ICategoriaDocumentoAPIService>("https://kufvqkxyedf7vml-db202003022030.adb.sa-saopaulo-1.oraclecloudapps.com/ords/cgarcia/hr");
-                Console.WriteLine("Consultando API");
-                var CategoriaDocumento = await CategoriaDocumentoClient.GetCategoriaDocumentoAsync();
-
-                Console.Write($"\nFirstName:{CategoriaDocumento.FirstName}");
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine("Erro na consulta " + e.Message);
-            }
-
-            
-
-            //StringBuilder sb = new StringBuilder();
-
-            //string file = @"C:\documentos_exportar\55014476 - THAYNA DEAMICI SIMAO0003.pdf";
-            //using(PdfReader reader = new PdfReader(file))
+            //try
             //{
-            //    for(int pageNo = 1; pageNo <=  reader.NumberOfPages; pageNo++)
-            //    {
+            //    var CategoriaDocumentoClient = RestService.For <ICategoriaDocumentoAPIService>("https://kufvqkxyedf7vml-db202003022030.adb.sa-saopaulo-1.oraclecloudapps.com/ords/cgarcia/hr");
+            //    Console.WriteLine("Consultando API");
+            //    var CategoriaDocumento = await CategoriaDocumentoClient.GetCategoriaDocumentoAsync();
 
-            //        ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
-            //        string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
-            //        text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default,Encoding.UTF8,Encoding.Default.GetBytes(text)));
-            //        sb.Append(text);
 
-            //    }
+            //    Console.Write($"\nFirstName:{CategoriaDocumento.FirstName}");
             //}
-          //  Console.WriteLine(sb.ToString());
-            Console.ReadKey();
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Erro na consulta " + e.Message);
+            //}
+            string folderPath = @"C:\documentos_exportar";
+            int cont = 0;
+
+            //string file = @"C:\documentos_exportar\55014476 - THAYNA DEAMICI SIMAO0001.pdf";
+
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.pdf"))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                using (PdfReader reader = new PdfReader(file))
+                {
+                    cont++;
+                    for(int pageNo = 1; pageNo <=  reader.NumberOfPages; pageNo++)
+                    {
+
+                        ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                        string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
+                        text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default,Encoding.UTF8,Encoding.Default.GetBytes(text)));
+                        sb.Append(text);
+
+                    }
+                    Console.WriteLine($"{cont}-------------------------------------------------------------------------------------------");
+                    Console.WriteLine(file);
+                    Console.WriteLine(sb.ToString());
+                   // Console.ReadKey();
+                   //string contents = File.ReadAllText(file);
+                }
+
+            }
 
         }
 
